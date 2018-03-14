59fb404ce5d280c86623f287d733041fc3f4ecda
Paul Vrugt <paulvrugt@gmail.com>
2018-03-14 17:18:00 +0100

    Moved Bulkexport download tool to subfolder, added c# example of calling bulkexport api

---
+++ Bulkexports/BulkExportDownloadTool/README.md
@@ -0,0 +1,1 @@
+Todo: Fill this
---
+++ Bulkexports/Download bulk export with c#.cs
@@ -0,0 +1,42 @@
+async public Task<BulkExport> DownloadBulkExportAsync(string id)
+{
+    var token = "<bearertoken>";
+    var apiKey = "<api key>";
+
+    Console.WriteLine("reading bulkexport " + id + " from API");
+    var client = new HttpClient();
+    var message = new HttpRequestMessage()
+    {
+        RequestUri = new Uri($"http://iprova/api/documents/{id}"),
+        Method = HttpMethod.Get
+    };
+
+    // easily add HTTP Headers
+    message.Headers.Add("Authorization", $"bearer {token}");
+    message.Headers.Add("x-api-version", "1");
+    message.Headers.Add("x-api_key", apiKey);
+
+    var response = await client.SendAsync(message, HttpCompletionOption.ResponseHeadersRead);
+
+    if (response.IsSuccessStatusCode)
+    {
+		//read the response JSON
+        var strData = await response.Content.ReadAsStringAsync();
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<BulkExport>(strData);
+    }
+    else
+        return null;
+}
+
+public class BulkExport
+{
+    public Int32 id { get; set; }
+
+    public string name { get; set; }
+
+    public string state { get; set; }
+
+    public string last_export_datetime { get; set; }
+
+    public bool can_download { get; set; }
+}
