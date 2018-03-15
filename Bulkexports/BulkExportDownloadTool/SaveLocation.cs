2926faddfad6255c74f4826945a51dca7e889917
Guy Bruens <20951114+GuyBruens@users.noreply.github.com>
2018-03-15 09:46:03 +0100

    Add files via upload
    Added rest of the project files

---
+++ Bulkexports/BulkExportDownloadTool/SaveLocation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkExportDownload
+{
+    class SaveLocation
+    {
+        public string id { get; set; }
+        public string location { get; set; }
+
+        public SaveLocation(string id, string location)
+        {
+            this.id = id;
+            this.location = location;
+        }
+    }
+}
