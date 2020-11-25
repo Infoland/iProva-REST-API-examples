1e289bd4ba9fc44bdf0925bc8396b1d336ca1c30
BastiaanMolsbeck <36535025+BastiaanMolsbeck@users.noreply.github.com>
2020-11-25 11:52:52 +0100

    Added method to download bulk export zip file

--- Bulkexports/Download bulk export with c#.cs
+++ Bulkexports/Download bulk export with c#.cs
@@ -1,4 +1,4 @@
-async public Task<BulkExport> DownloadBulkExportAsync(string id)
+async public Task<BulkExport> GetBulkExportDetailsAsync(string id)
 {
     var token = "<bearertoken>";
     var apiKey = "<api key>";
@@ -20,9 +20,50 @@ async public Task<BulkExport> DownloadBulkExportAsync(string id)
 
     if (response.IsSuccessStatusCode)
     {
-		//read the response JSON
+	//read the response JSON
         var strData = await response.Content.ReadAsStringAsync();
         return Newtonsoft.Json.JsonConvert.DeserializeObject<BulkExport>(strData);
+    }
+    else
+        return null;
+}
+
+async public Task<BulkExport> DownloadBulkExportAsync(string id, string zipPath)
+{
+    var token = "<bearertoken>";
+    var apiKey = "<api key>";
+
+    Console.WriteLine("reading bulkexport " + id + " from API");
+    var client = new HttpClient();
+    var message = new HttpRequestMessage()
+    {
+        RequestUri = new Uri($"http://iprova/api/documents/bulk_exports/{id}/download"),
+        Method = HttpMethod.Get
+    };
+
+    // easily add HTTP Headers
+    message.Headers.Add("Authorization", $"bearer {token}");
+    message.Headers.Add("x-api-version", "1");
+    message.Headers.Add("x-api_key", apiKey);
+
+    var response = await client.SendAsync(message, HttpCompletionOption.ResponseHeadersRead);
+
+    if (response.IsSuccessStatusCode)
+    {
+        using (var fileStream = new FileStream(zipPath, FileMode.Create))
+        {
+            // download bulk export zip file
+            await response.Content.CopyToAsync(fileStream);
+
+            if (fileStream.Length > 0)
+            {
+                blnSuccess = true; //success
+            }
+            else
+            {
+                blnSuccess = false;
+            }
+        }
     }
     else
         return null;
